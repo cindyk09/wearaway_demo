b50bafda902e135b4ce527bc1d4a5e3ba970e5bd
cindyk09 <cindyk09@gmail.com>
2017-06-22 15:51:15 -0400

    added custom errors and users crud (respond to json)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_token
   has_secure_password
 
-  validates :username, :email, :password, presence: true
+  validates :username, :email, :password, :password_confirmation, presence: true
   validates :username, length: { minimum: 5 }
   validates :email, :username, uniqueness: true
 
